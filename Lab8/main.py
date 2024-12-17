1b3e1e842f32b376c35790cd329e468a1db520b2
ArtemKorz1 <artemkorz@mail.ru>
2024-12-17 21:53:04 +0500

    Lab8 added
    - Added getting content of twitch root by:  locator, xpath, bs.
    - Added getting info after request + scrolling

--- Lab8/main.py
+++ Lab8/main.py
@@ -1,82 +1,154 @@
-import time
-
 from playwright.sync_api import Playwright, sync_playwright
+from lxml import html
+import bs4
 
-from Lab6.main import get_random_sleep
-from Lab6.login_const import login, password
-from Lab3.main import MongoWork
+from Lab1.main import JsonWork
 
-mongo = MongoWork()
-coll_name = 'mail_ru_with_playwright'
+lab8_suffix = 'Lab8\\'
+link = 'https://twitch.tv'
 
-def get_mails_by_playwright():
+
+def get_screenshot_by_playwright():
     with sync_playwright() as pw:
         browser = pw.firefox.launch(headless=False)
         page = browser.new_page()
-        page.goto('https://e.mail.ru/inbox/')
+        page.goto('https://twitch.tv/')
+        page.screenshot(path="twitch.jpg")
+        browser.close()
 
-        # input email
-        page.wait_for_load_state()
-        input = page.locator("//input[@name='username']")
-        submit = page.locator("//button[@class='base-0-2-79 primary-0-2-93 auto-0-2-105']")
-        time.sleep(get_random_sleep())
-        input.type(login, delay=0.2)
-        time.sleep(get_random_sleep())
-        submit.click()
 
-        time.sleep(get_random_sleep())
+def get_list_by_locator(page):
+    return page.locator("//div[@class='OBUFH ScTransitionBase-sc-hx4quq-0 tw-transition']")
 
-        # choose another method
-        submit = page.locator("//button[@class='base-0-2-79 fluid-0-2-86 auto-0-2-105']")
-        submit.click()
+def get_list_by_xpath(page):
+    content = html.fromstring(page.content())
+    list = content.xpath("//div[@class='OBUFH ScTransitionBase-sc-hx4quq-0 tw-transition']")
+    return list
 
-        time.sleep(get_random_sleep())
+def get_list_by_bs(page):
+    soup = bs4.BeautifulSoup(page.content(), "lxml")
+    list = soup.findAll("div", class_='OBUFH ScTransitionBase-sc-hx4quq-0 tw-transition')
+    return list
 
-        # input password
-        input = page.locator("//input[@name='password']")
-        submit = page.locator("//button[@class='base-0-2-79 primary-0-2-93 auto-0-2-105']")
-        time.sleep(get_random_sleep())
-        input.type(password, delay=0.2)
-        time.sleep(get_random_sleep())
-        submit.click()
+def get_element_by_locator(page, numb):
+    elem = {
+        'title': page.locator("//h3[@class='CoreText-sc-1txzju1-0 kBMZbI']").nth(numb).inner_text(),
+        'author': page.locator("//div[@class='Layout-sc-1xcs6mc-0 bQImNn']").nth(numb).inner_text(),
+        'category': page.locator("//a[@class='ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link']").nth(numb).inner_text(),
+        'viewers': page.locator("//div[@class='ScMediaCardStatWrapper-sc-anph5i-0 jRUNHm tw-media-card-stat']").nth(
+            numb).inner_text(),
+        'ref': link + page.locator("//a[@class='ScCoreLink-sc-16kq0mq-0 hcWFnG preview-card-image-link tw-link']").nth(
+            numb).get_attribute('href'),
+        'tags': page.locator("//div[@class='InjectLayout-sc-1i43xsx-0 gNgtQs']").nth(numb).inner_text().split('\n')
+    }
+    return elem
 
-        # wait to load page
-        page.locator("//div[@class='ReactVirtualized__Grid__innerScrollContainer']").wait_for()
-        time.sleep(1)
+def get_element_by_xpath(list, numb):
+    el = list[numb]
+    elem = {
+        'title': el.xpath(".//h3[@class='CoreText-sc-1txzju1-0 kBMZbI']/text()")[0],
+        'author': el.xpath(".//div[@class='Layout-sc-1xcs6mc-0 bQImNn']/text()")[0],
+        'category': el.xpath(".//a[@class='ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link']/text()")[0],
+        'viewers': el.xpath(".//div[@class='ScMediaCardStatWrapper-sc-anph5i-0 jRUNHm tw-media-card-stat']/text()")[0],
+        'ref': link + el.xpath(".//a[@class='ScCoreLink-sc-16kq0mq-0 hcWFnG preview-card-image-link tw-link']/@href")[0],
+        'tags': el.xpath(".//div[@class='ScTruncateText-sc-i3kjgq-0 ickTbV']/span/text()")
+    }
+    return elem
+
+def get_element_by_bs(list, numb):
+    el = list[numb]
+    tags_el = el.findAll("div", class_="ScTruncateText-sc-i3kjgq-0 ickTbV")
+    tags = []
+    for tag in tags_el:
+        tags += [tag.text]
+    elem = {
+        'title': el.find("h3", class_="CoreText-sc-1txzju1-0 kBMZbI").text,
+        'author': el.find("div", class_="Layout-sc-1xcs6mc-0 bQImNn").text,
+        'category': el.find("a", class_="ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link").text,
+        'viewers': el.find("div", class_="ScMediaCardStatWrapper-sc-anph5i-0 jRUNHm tw-media-card-stat").text,
+        'ref': link + el.find("a", class_="ScCoreLink-sc-16kq0mq-0 hcWFnG preview-card-image-link tw-link")['href'],
+        'tags': tags
+    }
+    return elem
+
+
+def get_content(method):
+    with sync_playwright() as pw:
+        browser = pw.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://twitch.tv')
+        page.locator("//div[@class='Layout-sc-1xcs6mc-0 cVWYqz']").wait_for()
+
+        parsed = []
+        if method == 'locator':
+            list = get_list_by_locator(page)
+        elif method == 'xpath':
+            list = get_list_by_xpath(page)
+        elif method == 'bs':
+            list = get_list_by_bs(page)
 
         numb = 0
-        mail_list = page.locator("//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a")
-        while True:
-            mail_list = page.locator("//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a")
-            time.sleep(get_random_sleep())
-            mail_list.nth(numb).click()
+        while numb < 6:
+            if method == 'locator':
+                elem = get_element_by_locator(page, numb)
+            elif method == 'xpath':
+                elem = get_element_by_xpath(list, numb)
+            elif method == 'bs':
+                elem = get_element_by_bs(list, numb)
 
-            page.locator("//h2[@class='thread-subject']").wait_for()
-
-            # get elements
-            title = page.locator("//h2[@class='thread-subject']").nth(0)
-            author = page.locator("//span[@class='letter-contact']").nth(0)
-            email = page.locator("//span[@class='letter-contact']").nth(0)
-            date = page.locator("//div[@class='letter__date']").nth(0)
-            text = page.locator("//div[@class='js-helper js-readmsg-msg']/div/div/div").nth(0)
-
-            elem = {
-                'title': title.inner_text(),
-                'author': author.inner_text(),
-                'email': email.get_attribute('title'),
-                'date': date.inner_text(),
-                'text': text.inner_text()
-            }
-
-            mongo.add(elem, ['title', 'author', 'email', 'date'], coll_name)
-
-            if mail_list.nth(numb) == mail_list.last:
-                break
-
-            time.sleep(get_random_sleep())
-            page.go_back()
-            page.locator("//div[@class='ReactVirtualized__Grid__innerScrollContainer']").wait_for()
-            time.sleep(1)
+            parsed += [elem]
+            print(elem)
             numb += 1
 
+        print(parsed)
+        JsonWork.save(lab8_suffix + "by_" + method + ".json", parsed)
+        browser.close()
+
+
+def get_with_request():
+    with sync_playwright() as pw:
+        browser = pw.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://twitch.tv')
+        input = page.locator("//input[@class='ScInputBase-sc-vu7u7d-0 ScInput-sc-19xfhag-0 gNGlOQ gnCxBd InjectLayout-sc-1i43xsx-0 eRDdjS tw-input tw-input--large']")
+        input.wait_for()
+        input.type("league of legends", delay=0.2)
+        page.locator("//button[@class='ScCoreButton-sc-ocjdkq-0 gFUsAR tw-combo-input__button-icon tw-combo-input__button-icon--large']").click()
+
+        page.locator("//p[@class='CoreText-sc-1txzju1-0 MveHm']").last.wait_for()
+        content = html.fromstring(page.content())
+        list = content.xpath("//div[@class='Layout-sc-1xcs6mc-0 ivrFkx']")
+        list.pop(0)
+        parsed = []
+        numb = 0
+        while numb < 10:
+            el = list[numb]
+            try:
+                title = el.xpath(".//p[@class='CoreText-sc-1txzju1-0 MveHm']/text()")[1]
+            except:
+                page.locator("//button[@class='ScCoreButton-sc-ocjdkq-0 gjSLzh']").nth(0).click()
+                page.locator("//p[@class='CoreText-sc-1txzju1-0 MveHm']").last.wait_for()
+                page.locator("//div[@class='Layout-sc-1xcs6mc-0 ivrFkx']").nth(numb).scroll_into_view_if_needed()
+                content = html.fromstring(page.content())
+                list = content.xpath("//div[@class='Layout-sc-1xcs6mc-0 ivrFkx']")
+                list.pop(0)
+                el = list[numb]
+                title = el.xpath(".//p[@class='CoreText-sc-1txzju1-0 MveHm']/text()")[1]
+
+            elem = {
+                'title': title,
+                'author': el.xpath(".//a[@class='ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link']/text()")[0],
+                'category': el.xpath(".//a[@class='ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link']/text()")[0],
+                'viewers':
+                    el.xpath(".//p[@class='CoreText-sc-1txzju1-0 MveHm']/text()")[0],
+                'ref': link +
+                       el.xpath(".//a[@class='ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link']/@href")[0],
+                'tags': el.xpath(".//div[@class='ScTruncateText-sc-i3kjgq-0 ickTbV']/span/text()")
+            }
+            parsed += [elem]
+            print(elem)
+            numb += 1
+
+        print(parsed)
+        JsonWork.save(lab8_suffix + "with_request.json", parsed)
         browser.close()
--- main.py
+++ main.py
@@ -7,33 +7,43 @@ from Lab4.spiders.tl_spider import coll_name as coll_name_available
 from Lab4.spiders.Experimental import coll_name as coll_name_protected
 from Lab4.spiders.quotes_spider import coll_name as coll_name_quotes
 from Lab6.main import get_mails_by_selenium
+from Lab8.mail import get_mails_by_playwright
+from Lab8.main import get_screenshot_by_playwright, get_content, get_with_request
 
 #Lab1
-Requestor.get_vk_groups()
-Requestor.get_git_repos()
+#Requestor.get_vk_groups()
+#Requestor.get_git_repos()
 
 #Lab2-3
-ParserTlPerm.get_stuff_by_xpath(True)
-ParserTlPerm.get_stuff_by_bsoup()
+#ParserTlPerm.get_stuff_by_xpath(True)
+#ParserTlPerm.get_stuff_by_bsoup()
 
 #Lab3
 #cost = input("Минимальная стоимость товара: ")
-cost = 100
-pprint(ParserTlPerm.choose_products_by_cost(cost))
+#cost = 100
+#pprint(ParserTlPerm.choose_products_by_cost(cost))
 
 #Lab4
 #README.md
-mongo = MongoWork()
-mongo.save_to_Json(coll_name_available, 'Lab4\\items_from_mongoDB_spider.json')
-mongo.save_to_Json(coll_name_protected, 'Lab4\\items_from_mongoDB_spider_protected.json')
+#mongo = MongoWork()
+#mongo.save_to_Json(coll_name_available, 'Lab4\\items_from_mongoDB_spider.json')
+#mongo.save_to_Json(coll_name_protected, 'Lab4\\items_from_mongoDB_spider_protected.json')
 
 #Lab5
 #README.md
-mongo = MongoWork()
-mongo.save_to_Json(coll_name_quotes, 'Lab5\\items_from_mongoDB_quotes_spider.json')
+#mongo = MongoWork()
+#mongo.save_to_Json(coll_name_quotes, 'Lab5\\items_from_mongoDB_quotes_spider.json')
 
 #Lab6
-get_mails_by_selenium()
+#get_mails_by_selenium()
 
 #Lab7
 #README.md
+
+#Lab8
+#get_mails_by_playwright()
+#get_screenshot_by_playwright()
+get_content('locator')
+get_content('xpath')
+get_content('bs')
+get_with_request()
